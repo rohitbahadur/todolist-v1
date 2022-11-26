f21af31f0cd173b0a1c6e91295b944e8cdbbd0ee
rbahadur <rohit.bahadur@diva-e.com>
2022-11-27 00:10:44 +0100

    implement raw get day

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 //jshint esversion:6
+//setting up our server
 
 const express = require("express");
 const bodyParser = require("body-parser");
@@ -6,9 +7,21 @@ const bodyParser = require("body-parser");
 const app = express();
 
 app.get("/", function (req, res) {
-	res.send("Hello");
+	//sending the browser the word hello when the user access the home route which is / in here (res is teh response from our server)
+
+	var day = new Date();
+	var currentDay = day.getDay();
+
+	if (currentDay === 6 || currentDay === 0) {
+		res.write("<h1>Yay! it weekend</h1>")
+	} else {
+		res.write("<p> Oh No, its weekeday <p>")
+		res.write("<h1> I have to work </h1>")
+	}
+	res.send();
 });
 
+// we are listening the app @port 3000
 app.listen(3000, function () {
 	console.log("Server started on port 3000.");
 });
