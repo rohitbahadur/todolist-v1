9e1afbedf211cfbd1d15429f22a725a76cdeb614
rbahadur <rohit.bahadur@diva-e.com>
2022-11-29 01:52:07 +0100

    added new route as /work

--- app.js
+++ app.js
@@ -39,21 +39,22 @@ app.get("/", function (req, res) {
 app.post("/", function (req, res) {
 	//add a new item, push the item back to server and from server back to browser (here the item get added on teh home route)
 	let item = req.body.newItem
-	items.push(item);
-	res.redirect("/");
+
+	if (req.body.list === "Work") {
+		workItems.push(item);
+		res.redirect("/work");
+	} else {
+		items.push(item);
+		res.redirect("/");
+	}
+
+
 
 	//console.log(item)
 })
 
 app.get("/work", function (req, res) {
 	res.render("list", { listTitle: "Work List", newListItems: workItems })
-})
-
-app.post("/work", function (req, res) {
-
-	let item = req.body.newItem
-	workItems.push(item);
-	res.redirect("/work")
 })
 
 
