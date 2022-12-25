c91e1a5d4a5413f8ac6bed226c6f4d54bbe27016
rbahadur <rohit.bahadur@diva-e.com>
2022-12-25 01:57:21 +0100

    chores

--- app.js
+++ app.js
@@ -1,74 +1,64 @@
 //jshint esversion:6
-//setting up our server
 
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const _ = require("lodash");
 
 const app = express();
+
 app.set('view engine', 'ejs');
-
-
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 mongoose.connect("mongodb://localhost:27017/todolistDB", { useNewUrlParser: true });
 
-//schema creation
 const itemsSchema = {
 	name: String
-}
+};
 
-//model for schema
 const Item = mongoose.model("Item", itemsSchema);
 
-const item1 = new Item({ name: "Apple" });
 
-const item2 = new Item({ name: "Banana" });
+const item1 = new Item({
+	name: "Apple"
+});
 
-const item3 = new Item({ name: "Mango" });
+const item2 = new Item({
+	name: "Banana"
+});
 
-//create a new array of items
+const item3 = new Item({
+	name: "Mango"
+});
+
 const defaultItems = [item1, item2, item3];
 
 app.get("/", function (req, res) {
-	//use Date method from java script to get day.
-	let today = new Date();
-
-	//js object to create a day with seeking information 
-	let options = {
-		weekday: "long",
-		day: "numeric",
-		month: "long",
-		hour: "2-digit",
-		minute: "2-digit"
-	}
-
-	let day = today.toLocaleDateString("en-ll", options)
-
-	//we pass in two variables kindOfDay and newListItems which we have in our ejs file
 
 	Item.find({}, function (err, foundItems) {
+
 		if (foundItems.length === 0) {
 			Item.insertMany(defaultItems, function (err) {
 				if (err) {
 					console.log(err);
 				} else {
-					console.log("Item Successfully Added");
+					console.log("Successfully savevd default items to DB.");
 				}
 			});
 			res.redirect("/");
 		} else {
-			res.render("list", { listTitle: day, newListItems: foundItems });
+			res.render("list", { listTitle: "Today", newListItems: foundItems });
 		}
 	});
+
 });
 
 // to add items in a form
 
 app.post("/", function (req, res) {
-	//add a new item, push the item back to server and from server back to browser (here the item get added on the home route)
+
 	const itemName = req.body.newItem;
 
 	const item = new Item({ name: itemName });
@@ -79,16 +69,30 @@ app.post("/", function (req, res) {
 
 app.post("/delete", function (req, res) {
 	const checkedItemId = req.body.checkbox;
+	const listName = req.body.listName;
 
-	Item.findOneAndDelete(checkedItemId, function (err) {
-		if (!err) {
-			res.redirect("/")
-		}
-	});
+	if (listName === "Today") {
+		Item.findByIdAndRemove(checkedItemId, function (err) {
+			if (!err) {
+				console.log("Successfully deleted checked item.");
+				res.redirect("/");
+			}
+		});
+	} else {
+		List.findOneAndUpdate({ name: listName }, { $pull: { items: { _id: checkedItemId } } }, function (err, foundList) {
+			if (!err) {
+				res.redirect("/" + listName);
+			}
+		});
+	}
+});
+
+app.get("/about", function (req, res) {
+	res.render("about");
 });
 
 
 // we are listening the app @port 3000
 app.listen(3000, function () {
-	console.log("Server started on port 3000.");
+	console.log("Server started on port 3000");
 });
